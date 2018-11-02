b6296bbf49340a6676eb89167728cbdd3c639f07
Erik Willert Hansen <erik3891@edu.eal.dk>
2018-11-02 11:08:50 +0100

    Add project files.

---
+++ Morgengry/MerchandiseRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class MerchandiseRepository
+    {
+        private List<Merchandise> merchandises = new List<Merchandise>();
+
+        public void AddAmulet(Merchandise merchandise)
+        {
+            merchandises.Add(merchandise);
+        }
+        public void AddBook(Merchandise merchandise)
+        {
+            merchandises.Add(merchandise);
+        }
+
+        public Merchandise Getamulet(string itemId)
+        {
+            for (int i = 0; i < merchandises.Count; i++)
+            {
+                if (merchandises[i].ItemId == itemId)
+                {
+                    return merchandises[i];
+                }
+            }
+            return null;
+        }
+
+        public double GetTotalValue()
+        {
+            double value = 0;
+            for (int i = 0; i < merchandises.Count; i++)
+            {
+                value += Utility.GetValueOfMerchandise(merchandises[i]);
+            }
+            return value;
+        }
+    }
+}
---
+++ Morgengry/Utility.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Utility
+    {
+        //public static double GetValueOfBook(Book book)
+        //{
+        //    return book.Price;
+        //}
+
+        public static double GetValueOfMerchandise(Merchandise merchandise)
+        {
+            if (merchandise is Amulet)
+            {
+                Amulet amulet = (Amulet)merchandise;
+                Level q = amulet.Quality;
+                // ændring
+                if (q == Level.low)
+                {
+                    return 12.5;
+                }
+                else if (q == Level.medium)
+                {
+                    return 20.0;
+                }
+                else
+                {
+                    return 27.5;
+                }
+
+            }
+            else
+            {
+                if (merchandise is Book)
+                {
+                    Book book = (Book)merchandise;
+                    return book.Price;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public static double GetValueOfCourse(Course course)
+        {
+            int DurationInHours = course.DurationInMinutes / 60;
+           
+            if (course.DurationInMinutes == 0)
+            {
+                return 0.0; 
+            }
+            else
+            {
+                return (DurationInHours + 1) * 875;
+            }
+        }
+    }
+}
